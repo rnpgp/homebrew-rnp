424367e046004756fce14dc614e242239840b86b
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-06-11 14:14:47 -0400

    Address Hound issues

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -1,29 +1,31 @@
 class Rnp < Formula
-  desc "OpenPGP (RFC 4880) tools, replacement of GnuPG"
-  homepage "https://github.com/riboseinc/rnp"
-  head "https://github.com/riboseinc/rnp.git"
+  desc 'OpenPGP (RFC 4880) tools, replacement of GnuPG'
+  homepage 'https://github.com/riboseinc/rnp'
+  head 'https://github.com/riboseinc/rnp.git'
 
-  depends_on "cmake" => :build
-  depends_on "json-c"
-  depends_on "botan"
+  depends_on 'cmake' => :build
+  depends_on 'json-c'
+  depends_on 'botan'
 
   def install
-    jsonc = Formula["json-c"]
-    botan = Formula["botan"]
+    jsonc = Formula['json-c']
+    botan = Formula['botan']
 
-    mkdir "build" do
-      system "cmake",
-        "-DBUILD_SHARED_LIBS=ON",
-        "-DBUILD_TESTING=OFF",
+    mkdir 'build' do
+      system(
+        'cmake',
+        '-DBUILD_SHARED_LIBS=ON',
+        '-DBUILD_TESTING=OFF',
         "-DCMAKE_INSTALL_PREFIX=#{prefix}",
         "-DCMAKE_PREFIX_PATH=#{botan.prefix};#{jsonc.prefix}",
         *std_cmake_args,
-        ".."
-      system "make", "install"
+        '..'
+      )
+      system 'make', 'install'
     end
   end
 
   test do
-    system "rnp", "--version"
+    system 'rnp', '--version'
   end
 end

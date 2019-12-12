acff22d3174a2c1cf09eeef83716aaec7f20e570
Roman Shevchenko <rrrooommmaaa@mail.ru>
2019-12-12 18:34:54 +0300

    echo to STDERR

--- ci/pushToBintray.sh
+++ ci/pushToBintray.sh
@@ -9,10 +9,14 @@ API=https://api.bintray.com
 # BOTTLE_VERSION
 BOTTLE="${BOTTLE_LOCAL_FILENAME}"
 
+echostatus() {
+  echo "$@" 1>&2;
+}
+
 main() {
   CURL="curl -u${BINTRAY_USER}:${BINTRAY_API_KEY} -H Content-Type:application/json -H Accept:application/json"
   if (check_package_exists); then
-    echo "The package ${BINTRAY_PACKAGE} does not exit. It will be created"
+    echostatus "The package ${BINTRAY_PACKAGE} does not exit. It will be created"
     create_package
   fi
 
@@ -20,14 +24,14 @@ main() {
 }
 
 check_package_exists() {
-  echo "Checking if package ${BINTRAY_PACKAGE} exists..."
+  echostatus "Checking if package ${BINTRAY_PACKAGE} exists..."
   package_exists=`[ $(${CURL} --write-out %{http_code} --silent --output /dev/null -X GET  ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}/${BINTRAY_PACKAGE})  -eq 200 ]`
-  echo "Package ${BINTRAY_PACKAGE} exists? y:1/N:0 ${package_exists}"
+  echostatus "Package ${BINTRAY_PACKAGE} exists? y:1/N:0 ${package_exists}"
   return ${package_exists}
 }
 
 create_package() {
-  echo "Creating package ${BINTRAY_PACKAGE}..."
+  echostatus "Creating package ${BINTRAY_PACKAGE}..."
   if [ -f "${BINTRAY_DESCRIPTOR_FILENAME}" ]; then
     data="@${BINTRAY_DESCRIPTOR_FILENAME}"
   else
@@ -44,17 +48,17 @@ create_package() {
 
 deploy_bottle() {
   if (upload_content); then
-    echo "Publishing ${BOTTLE}..."
+    echostatus "Publishing ${BOTTLE}..."
     ${CURL} -X POST ${API}/content/${BINTRAY_USER}/${BINTRAY_REPO}/${BINTRAY_PACKAGE}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
   else
-    echo "[SEVERE] First you should upload your bottle ${BOTTLE}"
+    echostatus "[SEVERE] First you should upload your bottle ${BOTTLE}"
   fi
 }
 
 upload_content() {
-  echo "Uploading ${BOTTLE}..."
+  echostatus "Uploading ${BOTTLE}..."
   uploaded=` [ $(${CURL} --write-out %{http_code} --silent --output /dev/null -T ${BOTTLE} -H X-Bintray-Package:${BINTRAY_PACKAGE} -H X-Bintray-Version:${PCK_VERSION} ${API}/content/${BINTRAY_USER}/${BINTRAY_REPO}/${BOTTLE}) -eq 201 ] `
-  echo "BOTTLE ${BOTTLE} uploaded? y:1/N:0 ${uploaded}"
+  echostatus "BOTTLE ${BOTTLE} uploaded? y:1/N:0 ${uploaded}"
   return ${uploaded}
 }
 

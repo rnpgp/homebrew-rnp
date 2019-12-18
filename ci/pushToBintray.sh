37b7be9f9bcefdfaf45d500151a0ce7476d90700
Roman Shevchenko <rrrooommmaaa@mail.ru>
2019-12-18 18:22:39 +0000

    better status handling

--- ci/pushToBintray.sh
+++ ci/pushToBintray.sh
@@ -16,24 +16,27 @@ echostatus() {
 
 main() {
   CURL="curl -u${BINTRAY_USER}:${BINTRAY_API_KEY} -H Content-Type:application/json -H Accept:application/json"
-  if (check_package_exists); then
+  if [ ! $(check_package_exists) ]; then
     echostatus "The package ${BINTRAY_PACKAGE} does not exit. It will be created"
-    create_package
-    if [ ! "$?" ]; then
+    if [ ! $(create_package) ]; then
       echostatus "The package could not be created"
-      return 0
+      exit 1
     fi
   fi
 
-  deploy_bottle
-  return $?
+  if [ ! $(deploy_bottle) ]; then
+    echostatus "Error deploying the bottle"
+    exit 2
+  fi
 }
 
 check_package_exists() {
   echostatus "Checking if package ${BINTRAY_PACKAGE} exists..."
-  package_exists=`[ $(${CURL} --write-out %{http_code} --silent --output /dev/null -X GET  ${API}/packages/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BINTRAY_PACKAGE})  -eq 200 ]`
-  echostatus "Package ${BINTRAY_PACKAGE} exists? y:1/N:0 ${package_exists}"
-  return ${package_exists}
+  output=$(${CURL} --write-out %{http_code} --silent --output /dev/null -X GET ${API}/packages/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BINTRAY_PACKAGE})
+  echostatus "Package check returned status $output"
+  if [ $output -eq 200 ]; then
+    echo 1
+  fi
 }
 
 create_package() {
@@ -50,31 +53,31 @@ create_package() {
   fi
   output=$(${CURL} --write-out %{http_code} --silent --output /dev/null -X POST -d "${data}" ${API}/packages/${BINTRAY_SUBJECT}/${BINTRAY_REPO})
   echostatus "Package upload returned status $output"
-  uploaded=` [ $output -eq 201 ] `
-  return ${uploaded}
+  if [ $output -eq 201 ]; then
+    echo 1
+  fi
 }
 
 deploy_bottle() {
-  if (upload_content); then
+  if [ $(upload_content) ]; then
     echostatus "Publishing ${BOTTLE}..."
-    result=` [ $(${CURL} --write-out %{http_code} --silent --output /dev/null -X POST ${API}/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BINTRAY_PACKAGE}/${BOTTLE_VERSION}/publish -d "{ \"discard\": \"false\" }") -eq 201 ] `
-    return ${result}
+    output=$(${CURL} --write-out %{http_code} --silent --output /dev/null -X POST ${API}/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BINTRAY_PACKAGE}/${BOTTLE_VERSION}/publish -d "{ \"discard\": \"false\" }")
+    echostatus "Content publish returned status $output"
+    if [ $output -eq 201 ]; then
+      echo 1
+    fi
   else
     echostatus "[SEVERE] First you should upload your bottle ${BOTTLE}"
-    return 0
   fi
 }
 
 upload_content() {
   echostatus "Uploading ${BOTTLE}..."
-  uploaded=` [ $(${CURL} --write-out %{http_code} --silent --output /dev/null -T ${BOTTLE} -H X-Bintray-Package:${BINTRAY_PACKAGE} -H X-Bintray-Version:${BOTTLE_VERSION} ${API}/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BOTTLE}) -eq 201 ] `
-  echostatus "BOTTLE ${BOTTLE} uploaded? y:1/N:0 ${uploaded}"
-  return ${uploaded}
+  output=$(${CURL} --write-out %{http_code} --silent --output /dev/null -T ${BOTTLE} -H X-Bintray-Package:${BINTRAY_PACKAGE} -H X-Bintray-Version:${BOTTLE_VERSION} ${API}/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BOTTLE})
+  echostatus "Content upload returned status $output"
+  if [ $output -eq 201 ]; then
+    echo 1
+  fi
 }
 
 main "$@"
-if [ ! "$?" ]; then
-  ! true
-else
-  true
-fi

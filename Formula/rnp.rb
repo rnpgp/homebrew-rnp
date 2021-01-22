0510003f577713d27b8a5ca302eb7a1d19f16a00
Roman Shevchenko <rrrooommmaaa@mail.ru>
2021-01-22 12:10:52 +0300

    v0.14.0

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -2,8 +2,8 @@ class Rnp < Formula
   desc "RNP is a set of OpenPGP (RFC4880) tools"
   homepage "https://github.com/rnpgp/rnp"
   url "https://github.com/rnpgp/rnp.git",
-      :tag => "v0.13.1",
-      :revision => "ad35f7c37467800a1b2b081fd80e1eb307dc2af9"
+      :tag => "v0.14.0",
+      :revision => "d1b6c731e99d3fd51fa130c87a8c9329a7c25406"
   head "https://github.com/rnpgp/rnp.git"
 
   depends_on "cmake" => :build

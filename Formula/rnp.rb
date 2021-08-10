3ca29bfb44a23b162aa81de52b83f2acc96128c1
Jeffrey Lau <2649467+ribose-jeffreylau@users.noreply.github.com>
2021-08-10 07:26:11 +0000

    rnp: add 0.15.2 bottle.

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -7,6 +7,11 @@ class Rnp < Formula
   sha256 "198f239808093312d61a54fcdde94aa147260867bde9ee521a31185bab34747e"
   head "https://github.com/rnpgp/rnp.git"
 
+  bottle do
+    root_url "https://github.com/rnpgp/homebrew-rnp/releases/download/rnp-0.15.2"
+    sha256 cellar: :any, catalina: "13cfb1132fe0c330e2ed2c5d613d27724768d81b68cc6470e65d71babb412278"
+  end
+
   depends_on "cmake" => :build
   depends_on "botan"
   depends_on "json-c"

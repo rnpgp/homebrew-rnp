738c872741c7f527b8c3985434a7a11cdac9b8ef
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-08-10 12:02:09 +0800

    lint: Fix some bash shellcheck issues

--- ci/read_bottle_metadata.sh
+++ ci/read_bottle_metadata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-export BOTTLE_VERSION=`cat *.bottle.json | jq --raw-output '.[].formula.pkg_version'`
-export BOTTLE_FILENAME=`cat *.bottle.json | jq --raw-output '.[].bottle.tags[].filename'`
-export BOTTLE_LOCAL_FILENAME=`cat *.bottle.json | jq --raw-output '.[].bottle.tags[].local_filename'`
-export BOTTLE_SHA256=`cat *.bottle.json | jq --raw-output '.[].bottle.tags[].sha256'`
+BOTTLE_VERSION=$(< ./*.bottle.json jq --raw-output '.[].formula.pkg_version')
+BOTTLE_FILENAME=$(< ./*.bottle.json jq --raw-output '.[].bottle.tags[].filename')
+BOTTLE_LOCAL_FILENAME=$(< ./*.bottle.json jq --raw-output '.[].bottle.tags[].local_filename')
+BOTTLE_SHA256=$(< ./*.bottle.json jq --raw-output '.[].bottle.tags[].sha256')
+
+export BOTTLE_VERSION="${BOTTLE_VERSION:?Missing BOTTLE_VERSION}"
+export BOTTLE_FILENAME="${BOTTLE_FILENAME:?Missing BOTTLE_FILENAME}"
+export BOTTLE_LOCAL_FILENAME="${BOTTLE_LOCAL_FILENAME:?Missing BOTTLE_LOCAL_FILENAME}"
+export BOTTLE_SHA256="${BOTTLE_SHA256:?Missing BOTTLE_SHA256}"

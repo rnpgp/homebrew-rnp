9315c9baf6db1d43af2dae17a142a7459f2d5d28
Maxim Samsonov <maxirmx@sw.consulting>
2023-04-15 12:31:12 +0300

    Revert "rnp reference updated to 0.16.3 (#32)" (#34)
    This reverts commit fd68e82630aa0f64126def8f4165db22b215684c.

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -3,34 +3,38 @@
 class Rnp < Formula
   desc "High-performance OpenPGP command-line tools and library"
   homepage "https://github.com/rnpgp/rnp"
-  url "https://github.com/rnpgp/rnp/archive/refs/tags/v0.16.3.tar.gz"
-  sha256 "5c4951e46cc29524a9eae90378414f88e6e0b54b59a1f44c75101b9022835e96"
-  license all_of: ["MIT", "BSD-2-Clause", "BSD-3-Clause"]
+  url "https://github.com/rnpgp/rnp/archive/refs/tags/v0.15.2.tar.gz"
+  sha256 "198f239808093312d61a54fcdde94aa147260867bde9ee521a31185bab34747e"
   head "https://github.com/rnpgp/rnp.git"
+
+  bottle do
+    root_url "https://github.com/rnpgp/homebrew-rnp/releases/download/rnp-0.15.2"
+    sha256 cellar: :any, catalina: "13cfb1132fe0c330e2ed2c5d613d27724768d81b68cc6470e65d71babb412278"
+  end
 
   depends_on "cmake" => :build
   depends_on "botan"
   depends_on "json-c"
 
   def install
-    system "cmake", "-S", ".", "-B", "build",
-           "-DBUILD_TESTING=OFF", *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    mkdir "build" do
+      system(
+        "cmake",
+        "..",
+        *std_cmake_args,
+      )
+      system "make", "install"
+    end
   end
 
   test do
-    testin = testpath / "message.txt"
+    testin = testpath/"message.txt"
     testin.write "hello"
-    encr = "#{testpath}/enc.rnp"
-    decr = "#{testpath}/dec.rnp"
-    shell_output("#{bin}/rnpkeys --generate-key --password=PASSWORD")
-    shell_output(
-      "#{bin}/rnp -c --password DUMMY --output #{encr} #{testin}",
-    )
-    shell_output(
-      "#{bin}/rnp --decrypt --password DUMMY --output #{decr} #{encr}",
-    )
-    cmp testin, decr
+    encrypted = testpath/"enc.rnp"
+    decrypted = testpath/"dec.rnp"
+    shell_output("rnpkeys --generate-key --password=PASSWORD")
+    shell_output("rnp -c --password DUMMY --output #{encrypted} #{testin}")
+    shell_output("rnp --decrypt --password DUMMY --output #{decrypted} #{encrypted}")
+    cmp testin, decrypted
   end
 end

8dc02faa0a8367e2c27b8fc9d165be3735f03e2a
Nickolay Olshevsky <o.nickolay@gmail.com>
2024-05-16 15:09:25 +0300

    Update formula to v0.17.1

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class Rnp < Formula
-  desc "High-performance OpenPGP command-line tools and library"
+  desc "High performance C++ OpenPGP library used by Mozilla Thunderbird"
   homepage "https://github.com/rnpgp/rnp"
-  url "https://github.com/rnpgp/rnp/archive/refs/tags/v0.16.3.tar.gz"
-  sha256 "5c4951e46cc29524a9eae90378414f88e6e0b54b59a1f44c75101b9022835e96"
+  url "https://github.com/rnpgp/rnp/releases/download/v0.17.1/rnp-v0.17.1.tar.gz"
+  sha256 "3095e2439ecb7b65f0ab72b7bbfafeb39bb1dda3c24c2ae110719a41bbfbf37d"
   license all_of: ["MIT", "BSD-2-Clause", "BSD-3-Clause"]
-  head "https://github.com/rnpgp/rnp.git"
+  head "https://github.com/rnpgp/rnp.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "botan"
   depends_on "json-c"
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
 
   def install
     system "cmake", "-S", ".", "-B", "build",
